300bc042280900f7a60d7d03cdb4404c7dc891f1
Ayush <ag8350961@gmail.com>
2023-04-24 11:56:16 +0530

    typing anime js

---
+++ main.js
@@ -0,0 +1,20 @@
+window.addEventListener('load',function (){
+    setTimeout(auth,2000);
+    console.log('ok')
+  });
+var idx = 0
+var ans = ""
+function helper(){
+  var text="Hello! I am Ayush.Nice to meet you."
+  if(idx==text.length){
+    idx=0;
+    ans=""
+    return
+  }
+  ans=ans+text[idx]
+  document.getElementById('typingAnime').innerHTML=ans+"|"
+  idx++
+}
+function auth(){
+  setInterval(helper,100)
+}
